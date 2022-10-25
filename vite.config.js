847808ce005c665d88420c35acf90246b4b07f23
Siluetaz <ysmaelborquezz@gmail.com>
2022-10-24 23:22:30 -0300

    readme

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/interactive-card-frontend-mentor/dist/',
-  plugins: [vue()]
-})
